e4daaf72d0299198c862bf0e72ca9b378a82bce1
Sameer Faridi <s.faridi007@gmail.com>
2023-08-06 13:31:08 +0530

    starting to make server of spacevoyage

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,10 @@
-const PORT = process.env.PORT || 8800;
+const http = require("http");
+const app = require("./app");
+const PORT = process.env.PORT || 6000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}...`);
+});
+
